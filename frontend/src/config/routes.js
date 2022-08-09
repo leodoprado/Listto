b782491fea0c4c25360b273323bbe3b8895961bb
Leonardo do Prado <leonardodoprado231@gmail.com>
2022-08-09 09:42:09 -0300

    Atualizando projeto

---
+++ frontend/src/config/routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "../pages/Home/HomePage";
+import LoginPage from "../pages/Login/LoginPage";
+
+export default function Router() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<HomePage />}></Route>
+                <Route exact path="/login" element={<LoginPage />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
