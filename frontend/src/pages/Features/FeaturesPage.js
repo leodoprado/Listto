06e0bf8ffd9b043ec79f74db2340b047f559c5ad
Leonardo do Prado <leonardodoprado231@gmail.com>
2022-08-15 13:39:50 -0300

    feat: changing Features page

--- frontend/src/pages/Features/FeaturesPage.js
+++ frontend/src/pages/Features/FeaturesPage.js
@@ -3,11 +3,26 @@ import React from "react";
 import Header from "../../components/Header/HeaderComponent";
 import Footer from "../../components/Footer/FooterComponent";
 
+import { ContainerFeaturesPage, ContainerLeft, ContainerRight, ContainerConteudo } from "./FeaturesPageStyle";
+
+import { AiOutlineRightCircle } from "react-icons/ai";
+
 const FeaturesPage = () => {
   return (
     <>
         <Header />
-            <div>FeaturesPage</div>
+          <ContainerFeaturesPage>
+            <ContainerLeft>
+              <h3> <AiOutlineRightCircle /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nemo? Necessitatibus culpa pariatur possimus nobis, unde adipisci suscipit, quasi, quo quaerat numquam doloribus sunt ratione dignissimos dolore laudantium eligendi tempora?</h3>
+              <br />
+              <h3> <AiOutlineRightCircle /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nemo? Necessitatibus culpa pariatur possimus nobis, unde adipisci suscipit, quasi, quo quaerat numquam doloribus sunt ratione dignissimos dolore laudantium eligendi tempora?</h3>
+            </ContainerLeft>
+            <ContainerRight>
+              <h3> <AiOutlineRightCircle /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nemo? Necessitatibus culpa pariatur possimus nobis, unde adipisci suscipit, quasi, quo quaerat numquam doloribus sunt ratione dignissimos dolore laudantium eligendi tempora?</h3>
+              <br />
+              <h3> <AiOutlineRightCircle /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nemo? Necessitatibus culpa pariatur possimus nobis, unde adipisci suscipit, quasi, quo quaerat numquam doloribus sunt ratione dignissimos dolore laudantium eligendi tempora?</h3>
+            </ContainerRight>
+          </ContainerFeaturesPage>
         <Footer />
     </>
   )
---
+++ frontend/src/pages/Features/FeaturesPageStyle.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const ContainerFeaturesPage = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Figtree', sans-serif;
+
+    br {
+        margin-bottom: 30px;
+    }
+`
+
+export const ContainerLeft = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+    width: 40vw;
+    height: 40vh;
+    color: black;
+    margin-right: 40px;
+    color: #46B241;
+`
+
+export const ContainerRight = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+    width: 40vw;
+    height: 40vh;
+    color: black;
+    margin-left: 40px;
+    color: #46B241;
+`
